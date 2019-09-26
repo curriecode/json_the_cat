e86ea74186ebbf73b0edfe4ccbe3a3529c2835d9
curriecode <melaniecurrie01@gmail.com>
2019-09-26 13:05:58 -0700

    "refactoring complete, everything working as expected"

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,33 @@
 const request = require('request');
 const args = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], (err, response, body) => {
-  if (err) {
-    throw new Error("woops something is wrong with the request");
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Sorry that cat breed is not available in our system, please try again :) ");
-    return;
-  }
-  console.log(data[0].description);
-});
+// request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], (err, response, body) => {
+//   if (err) {
+//     throw new Error("woops something is wrong with the request");
+//   }
+//   const data = JSON.parse(body);
+//   if (data.length === 0) {
+//     console.log("Sorry that cat breed is not available in our system, please try again :) ");
+//     return;
+//   }
+//   console.log(data[0].description);
+// });
+
+const fetchBreedDescription = (breedName, callback) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (err, response, body) => {
+    if (err) {
+      throw new Error("woops something is wrong with the request");
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      err = "Breed not found";
+      callback(err);
+      // return;
+    } else {
+      callback(err, data[0]['description']);
+    }
+  });
+};
+// console.log(fetchBreedDescription());
+
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,10 @@
+const fetchBreedDescription = require('./breedFetcher');
+const args = process.argv.slice(2);
+
+fetchBreedDescription(args[0], (err, desc) => {
+  if (err) {
+    console.log('Error fetch details:', err);
+  } else {
+    console.log(desc);
+  }
+});
